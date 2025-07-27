6b7c5eb5a0662069536f2bd5af51039f5cde77c7
Julian R <alobuenastarde@gmail.com>
2025-07-27 16:40:20 -0500

    Add examples of JavaScript data types
    Created tiposDeDatos.js with examples and console outputs for Number, String, Boolean, Null, and Undefined types to illustrate basic JavaScript data types.

---
+++ tiposDeDatos.js
@@ -0,0 +1,20 @@
+//Number - almacena números enteros y decimales
+let miNumero3 = 25.5;
+console.log(miNumero3);
+
+//String - almacena texto
+let miTexto = "Hola, mundo!";
+console.log(miTexto);
+
+//Boolean - almacena valores de verdad (true o false)
+let miBooleano = true;
+console.log(miBooleano);
+
+//Null - representa la ausencia de la referencia a un objeto
+// Se usa para indicar que una variable no tiene valor
+let miNulo = null;
+console.log(miNulo);
+
+//Undefined - indica que una variable no ha sido asignada
+let miIndefinido;
+console.log(miIndefinido); // Imprime 'undefined'
