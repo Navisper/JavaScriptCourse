fe5246cea4be59a4122bece7f20abdf58cc52760
Julian R <alobuenastarde@gmail.com>
2025-07-27 16:42:55 -0500

    Update tiposDeDatos.js

--- tiposDeDatos.js
+++ tiposDeDatos.js
@@ -1,28 +1,35 @@
 //Number - almacena números enteros y decimales
 let miNumero3 = 25.5;
 console.log(miNumero3);
+console.log(typeof miNumero3); // Imprime 'number'
 
 //Entero - almacena números enteros
 let miEntero = 10;
 console.log(miEntero);
+console.log(typeof miEntero); // Imprime 'number'
 
 //Float - almacena números decimales
 let miDecimal = 3.14;
 console.log(miDecimal);
+console.log(typeof miDecimal); // Imprime 'number'
 
 //String - almacena texto
 let miTexto = "Hola, mundo!";
 console.log(miTexto);
+console.log(typeof miTexto); // Imprime 'string'
 
 //Boolean - almacena valores de verdad (true o false)
 let miBooleano = true;
 console.log(miBooleano);
+console.log(typeof miBooleano); // Imprime 'boolean'
 
 //Null - representa la ausencia de la referencia a un objeto
 // Se usa para indicar que una variable no tiene valor
 let miNulo = null;
 console.log(miNulo);
+console.log(typeof miNulo); // Imprime 'object' (es un comportamiento peculiar de JavaScript)
 
 //Undefined - indica que una variable no ha sido asignada
 let miIndefinido;
 console.log(miIndefinido); // Imprime 'undefined'
+console.log(typeof miIndefinido); // Imprime 'undefined'
