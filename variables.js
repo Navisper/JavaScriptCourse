c76fbf401a569bf99ed4513f359e0cc3d81b4042
Julian R <alobuenastarde@gmail.com>
2025-07-26 21:02:47 -0500

    Add comments on variable declaration rules in JS
    Added detailed comments explaining the rules for declaring variables in JavaScript, including naming conventions and allowed characters. Also included example variable declarations to illustrate correct usage.

--- variables.js
+++ variables.js
@@ -6,3 +6,25 @@ console.log(miNumero1);
 // Forma moderna de declarar variables
 let miNumero2 = 20;
 console.log(miNumero2);
+
+//Reglas para declarar variables
+// 1. No se puede iniciar con un número
+//let 1numero = 10; // Esto es incorrecto
+//let numero1 = 10; Esto es correcto
+// 2. No se pueden usar palabras reservadas
+// 3. No se pueden usar caracteres especiales, 
+// excepto el guion bajo (_)
+// y el signo de dólar ($)
+// 4. Las variables son sensibles a mayúsculas y minúsculas
+// 5. Se pueden usar letras, números, guiones bajos y signos de dólar
+// 6. No se pueden usar espacios en blanco
+// 7. Se recomienda usar nombres descriptivos y significativos
+// 8. Las variables deben ser únicas en su ámbito
+// 9. Las variables usan camelCase para nombres compuestos
+
+//ejemplos 
+let $miVariable = 10; // Correcto
+
+let _miVariable = 20; // Correcto
+
+let miVariable1 = 30; // Correcto
