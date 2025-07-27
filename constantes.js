795b8dd5d6e7543c4675b3e4ace7eec5db64bde2
Julian R <alobuenastarde@gmail.com>
2025-07-27 16:51:40 -0500

    Add constantes.js with PI constant example
    Introduces a new file constantes.js demonstrating the declaration and usage of a constant (PI) in JavaScript, including explanatory comments.

---
+++ constantes.js
@@ -0,0 +1,8 @@
+//Constante
+//es una variable cuyo valor no puede cambiar una vez asignado
+// se declara con la palabra clave const
+// Las constantes deben ser inicializadas al momento de su declaración
+// y no pueden ser reasignadas posteriormente
+// Las constantes usan mayúsculas por convención para diferenciarlas de las variables
+const PI = 3.14159; // Declaración de una constante
+console.log(PI); // Imprime 3.14159
